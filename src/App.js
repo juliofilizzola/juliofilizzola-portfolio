0be7ff26ffb201450c8ee68596a8204b3a6329c7
Filizzola <juliofilizzola@hotmail.com>
2021-08-17 19:46:50 -0300

    configurando o Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Home from './pages/Home';
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-     <Home />
+      <Switch >
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
