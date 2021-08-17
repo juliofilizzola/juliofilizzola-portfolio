a68666101e07c8a49ee8266cd26912d0b672b9c6
Filizzola <juliofilizzola@hotmail.com>
2021-08-17 18:39:10 -0300

    importando footer no Home

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      Olá, esse é o Footer
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 function Home() {
@@ -5,6 +6,7 @@ function Home() {
     <div>
       <Header />
       olá
+      <Footer />
     </div>
   );
 }
