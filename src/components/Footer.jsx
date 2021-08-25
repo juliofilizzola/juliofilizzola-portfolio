5176c0511e405bb8f1cbe6936778d120915862d9
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 10:12:06 -0300

    importando reactIcons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import { BsHeartFill } from 'react-icons/bs';
+
 function Footer() {
   return (
     <footer>
-      Pagina feita com React e Bootstrap. since 2021 
+      Pagina feita com React e Bootstrap. since 2021 <BsHeartFill/>
     </footer>
   );
 }
