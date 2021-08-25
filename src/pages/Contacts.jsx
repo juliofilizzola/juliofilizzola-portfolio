6f7cf48fdeb14caab596e8df5d8b14e2ce417c21
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 10:28:42 -0300

    estruturando contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,31 @@
 function Contacts() {
   return (
     <div>
-      Contacts
+      <div>
+        <p>Contact Me</p>
+        <div>
+          LinkedIn
+          Twitter
+          Instagram  
+        </div> 
+        <div>
+          <p>Ou me mande um email</p>
+          <form action="">
+            <label>
+              <input type="text" name="email" value="<?php>" />
+            </label>
+            <label>
+              <input type="text" name="email" value="<?php>" />
+            </label>
+            <label>
+              <input type="text" name="email" value="<?php>" />
+            </label>
+            <label>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </label>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
