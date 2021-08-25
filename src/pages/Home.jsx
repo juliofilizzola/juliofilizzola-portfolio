d546cce871f7b2862bd8134a07bc587d3a23f1e3
Filizzola <juliofilizzola@hotmail.com>
2021-08-25 09:22:06 -0300

    importando header e footer no home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
 function Home() {
   return (
     <div>
+      <Header />
       Ola
+      <Footer />
     </div>
   );
 }
