7bab77759800898ca48f7bed90f1257d9357d0a4
Filizzola <juliofilizzola@hotmail.com>
2021-08-30 17:24:18 -0300

    centralizando gif

--- src/components/AboutMyself.jsx
+++ src/components/AboutMyself.jsx
@@ -4,6 +4,7 @@ import './aboutMyself.scss';
 function AboutMyself() {
   return (
     <div  className="container-myself">
+      <img src={'https://i2.wp.com/media.giphy.com/media/KziKCpvrGngHbYjaUF/giphy.gif?w=800&ssl=1'} alt="baby yoda" />
       <h1>Um pouco sobre mim</h1>
       <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         <Link to='/about'>PARA Saber mais...</Link>
