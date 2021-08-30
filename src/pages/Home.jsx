323f1f8db5328bcbac62c4134542ab11e4330791
Filizzola <juliofilizzola@hotmail.com>
2021-08-30 16:47:03 -0300

    create projecReview

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import AboutMyself from "../components/AboutMyself";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import ProjectReview from "../components/ProjectReview";
 
 function Home() {
   return (
     <div>
       <Header />
       <AboutMyself />
+      <ProjectReview />
       <Footer />
     </div>
   );
