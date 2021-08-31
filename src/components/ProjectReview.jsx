85bc45e0708573c2cb8c821e3e04994190b3556f
Filizzola <juliofilizzola@hotmail.com>
2021-08-31 10:12:47 -0300

    edit ProjectReview

--- src/components/ProjectReview.jsx
+++ src/components/ProjectReview.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './projectReview.scss';
 
 function ProjectReview() {
   return (
     <div className="projectReview">
       <h1>Projetos</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <Link to="/projects">Veja meus projetos</Link>
+      </p>
     </div>
   );
 }
