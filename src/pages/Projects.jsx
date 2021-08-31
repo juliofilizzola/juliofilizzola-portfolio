7512ce5e36cf904cb21e851df376eb1278c1e183
Filizzola <juliofilizzola@hotmail.com>
2021-08-31 20:13:02 -0300

    add resumo da pagina projetos e também colocando o projeto Todo list

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Space from '../image/block_big.jpg';
+import Todo from '../image/todoList.png'
 // import { Card } from 'react-bootstrap';
 import './projects.scss';
 
@@ -13,7 +14,7 @@ function Projects() {
         <main>
           <h1>My Projects</h1>
           <div>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+            Algum desses projetos são de conho avaliativo da Trybe que fiz, e outros são projetos pessoais.
           </div>
           <p>Link to my 
             <a href="https://github.com/juliofilizzola" target="_blank" rel="noreferrer">
@@ -29,11 +30,14 @@ function Projects() {
           <div className="Project">
             <h3>Todo-List</h3> 
             <div className="containerProject">
-              <img className="img-projects" src={Space} alt='imagem do projeto'/>
+              <img className="img-projects" src={Todo} alt='imagem do projeto'/>
+              <div className="text_Container">
+              <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end;</p>
               <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-              <span>Repositorio git</span> <span>site</span>
+                Esse projeto foi feito em React, usnado as tecnologia de React Router Dom, React Redux e foi estilizado usando CSS.
+                <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio</a>
               </p>
+              </div>
             </div>
           </div>
           <div className="Project">
