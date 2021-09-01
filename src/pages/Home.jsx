5fe25574132823f87c5a9b4d27a0508ac9e8be7b
Filizzola <juliofilizzola@hotmail.com>
2021-08-31 21:53:03 -0300

    mudando title da pagina de home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { useLocation } from 'react-router-dom';
 import AboutMyself from "../components/AboutMyself";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ProjectReview from "../components/ProjectReview";
 
 function Home() {
+  const location = useLocation();
+  
+  React.useEffect(() => {
+    if (location.pathname === '/'){
+      document.title= "Julio Filizzola";
+
+      // document.getElementById("favicon").href = <FaBeer/>;
+    }
+  });
   return (
     <div>
       <Header />
