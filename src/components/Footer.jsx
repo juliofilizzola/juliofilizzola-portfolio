e93d5f2c17f4f9aca7df2d8d28960bf15196e69c
Filizzola <juliofilizzola@hotmail.com>
2021-08-31 21:32:01 -0300

    add icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import { BsHeartFill } from 'react-icons/bs';
+import { DiSass, DiReact } from "react-icons/di";
+
 import './footer.scss';
 
 function Footer() {
   return (
     <footer>
-      <p>Pagina feita com React e CSS. since 2021 <BsHeartFill/></p>
+      <p>Pagina feita com <DiReact/> e <DiSass/>. since 2021 <BsHeartFill/></p>
     </footer>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import Footer from "../components/Footer"
+import {FaBeer} from 'react-icons/fa';
+import {DiGithubBadge} from 'react-icons/di';
 import { useLocation } from 'react-router-dom';
-import Header from "../components/Header"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Space from '../image/block_big.jpg';
-import Todo from '../image/todoList.png'
+import Todo from '../image/todoList.png';
 import './projects.scss';
 
 
@@ -13,12 +15,13 @@ function Projects() {
   React.useEffect(() => {
     if (location.pathname === '/projects'){
       document.title='Projects';
-      document.getElementById("favicon").href = Todo;
+      document.getElementById("favicon").href = <FaBeer/>;
     }
   })
   return (
     <div>
       <Header />
+      
       <div className="project-container">
         <main>
           <h1>Meus projetos</h1>
@@ -44,7 +47,7 @@ function Projects() {
               <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end;</p>
               <p>
                 Esse projeto foi feito em React, usnado as tecnologia de React Router Dom, React Redux e foi estilizado usando CSS.
-                <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio</a>
+                <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
               </p>
               </div>
             </div>
@@ -56,7 +59,7 @@ function Projects() {
               Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
               <span>Repositorio git</span> <span>site</span>
               </p>
-              <img className="img-projects" src={Space} alt='imagem do projeto'/>
+              <img className="img-projects" src={Todo} alt='imagem do projeto'/>
             </div>
           </div>
           <div className="Project">
