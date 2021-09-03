f96a99adc78f22f8bdc04d185694944692158f22
Filizzola <juliofilizzola@hotmail.com>
2021-09-03 18:32:45 -0300

    edit aboutMyself

--- src/components/AboutMyself.jsx
+++ src/components/AboutMyself.jsx
@@ -6,8 +6,9 @@ function AboutMyself() {
     <div  className="container-myself">
       <img src={'https://i2.wp.com/media.giphy.com/media/KziKCpvrGngHbYjaUF/giphy.gif?w=800&ssl=1'} alt="baby yoda" />
       <h1>Um pouco sobre mim</h1>
-      <p> Olá, Meu nome é Julio Filizzola
-        <Link to='/about'>PARA Saber mais...</Link>
+      <p>
+      Olá, meu nome é Julio Filizzola, sou estudante de Web desenvolvimento na Trybe. Usei todos os meu conhecimentos adquiridos lá para criar esse portfólio. 
+        <Link to='/about'>Para Saber Mais sobre mim...</Link>
       </p>
     </div>
   )
