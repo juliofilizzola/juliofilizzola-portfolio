27fa920c67e223e7d6d7151e1aabceb1f54e5ae5
juliofilizzola <juliofilizzola@Hotmail.com>
2021-09-04 19:37:23 -0300

    apagando importação do Link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Skills from "../components/Skills";
