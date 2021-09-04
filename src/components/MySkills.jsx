c7de68f5b473a4e8a50375a77f5ff3d23a77d1de
Filizzola <juliofilizzola@hotmail.com>
2021-09-04 10:30:02 -0300

    edit text

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 function MySkills() {
   return (
     <div className="my-skill-review">
-      
+      <h1>Skills</h1>
+      <p>
+        Minhas Skills, que adiquiri durante os estudos na Trybe!
+        <Link to="/projects">Veja elas por completo!</Link>
+      </p>
     </div>
   );
 }
