442049e44b432aaeb20c37fc4e71776feb1d9123
Filizzola <juliofilizzola@hotmail.com>
2021-09-04 10:08:39 -0300

    edit text

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -47,7 +47,7 @@ function Projects() {
             <div className="containerProject">
               <img className="img-projects" src={Todo} alt='imagem do projeto'/>
               <div className="text_Container">
-              <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end;</p>
+              <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe;</p>
               <p>
                 Esse projeto foi feito em React, usanado as tecnologia de React Router Dom, React Redux e foi estilizado usando CSS.
                 <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
