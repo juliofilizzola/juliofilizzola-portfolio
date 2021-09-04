9224b416a19f407600209d255995a19181e14f33
Filizzola <juliofilizzola@hotmail.com>
2021-09-04 12:32:56 -0300

    importanto icon

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './skills.css';
+import {DiHtml5, 
+  DiCss3, DiGit, DiGithubBadge, DiReact, DiSass, DiNpm, DiJsBadge} from "react-icons/di";
 
 function Skills() {
    
@@ -11,7 +13,10 @@ function Skills() {
         <div className="skills-hard">
           <h1>Languages</h1>
           <div>
-            JAVASCRIPT
+            <DiHtml5 />
+            <DiJsBadge />
+            <DiCss3 />
+
           </div>
         </div>
 
@@ -20,7 +25,11 @@ function Skills() {
 
           <h2> Frameworks</h2>
           <div>
-            JAVASCRIPT
+            <DiGithubBadge />
+            <DiReact />
+            <DiSass />
+            <DiNpm />
+            <DiGit />
           </div>
         </div>
       </div>
