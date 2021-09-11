3aa1dfa7be47d98a8cb9a96b5a24a60cbcdfd2fb
juliofilizzola <juliofilizzola@Hotmail.com>
2021-09-11 18:34:58 -0300

    centralizando o texto na tela até 600px de tamanho

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -43,7 +43,7 @@ function Projects() {
         </main>
         <section>
           <div className="Project">
-            <h3>To-do List</h3> 
+            <h3 className="h3Project">To-do List</h3> 
             <div className="containerProject">
               <img className="img-projects" src={Todo} alt='imagem do projeto'/>
               <div className="text_Container">
@@ -56,7 +56,7 @@ function Projects() {
             </div>
           </div>
           <div className="Project">
-            <h3>Pokebola</h3> 
+            <h3 className="h3Project">Pokebola</h3> 
             <div className="containerProject">
               <div className="text_Container">
                 <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end;
@@ -70,17 +70,6 @@ function Projects() {
                 <img className="img-projects" src={Pokebola} alt='imagem do projeto'/>
               </div>
           </div>
-          <div className="Project">
-            <h3>Todo-List</h3> 
-            <div className="containerProject">
-              <img className="img-projects" src={Space} alt='imagem do projeto'/>
-              <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-              <span>Repositorio git</span> <span>site</span>
-              </p>
-            </div>
-          </div>
-
    
         </section>
       </div>
