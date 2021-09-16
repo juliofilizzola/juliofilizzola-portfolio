930cc00876396226d7da2dd9d3db8304783fd0bd
Filizzola <juliofilizzola@hotmail.com>
2021-09-16 12:04:10 -0300

    projeto Marvel Hero

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,13 +5,9 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Todo from '../image/todoList.png';
 import ProjectsImg from '../image/project.png';
-import Pokebola from "../image/pokebola.png";
+import Avenges from "../image/avengers.png";
 import shoppingCart from "../image/shopping-cart.png";
 import './projects.css';
-
-
-
-
 
 function Projects() {
   const location = useLocation();
@@ -75,6 +71,22 @@ function Projects() {
               </div>
           </div>
    
+          <div className="Project">
+            <h3 className="h3Project">Marvel Heros</h3> 
+            <div className="containerProject">
+              <img className="img-projects" src={Avenges} alt='imagem do projeto'/>
+              <div className="text_Container">
+              <p>
+                O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe.
+
+              </p>
+              <p>
+                Esse projeto foi feito em React, usando as tecnologias: de React Router Dom, React Redux e foi estilizado usando CSS.
+                <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
+              </p>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
       <Footer />
