3ab09ef0d809764714cc045876131b555ac0445e
Filizzola <juliofilizzola@hotmail.com>
2021-09-16 11:53:33 -0300

    edit image do shopping cart

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,10 @@ import Footer from "../components/Footer";
 import Todo from '../image/todoList.png';
 import ProjectsImg from '../image/project.png';
 import Pokebola from "../image/pokebola.png";
+import shoppingCart from "../image/shopping-cart.png";
 import './projects.css';
+
+
 
 
 
@@ -46,7 +49,10 @@ function Projects() {
             <div className="containerProject">
               <img className="img-projects" src={Todo} alt='imagem do projeto'/>
               <div className="text_Container">
-              <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe.</p>
+              <p>
+                O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe.
+
+              </p>
               <p>
                 Esse projeto foi feito em React, usando as tecnologias: de React Router Dom, React Redux e foi estilizado usando CSS.
                 <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
@@ -65,7 +71,7 @@ function Projects() {
                   <a href="https://github.com/juliofilizzola/site-de-compras" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
                 </p>
                 </div>
-                <img className="img-projects" src={Pokebola} alt='imagem do projeto'/>
+                <img className="img-projects" src={shoppingCart} alt='imagem do projeto'/>
               </div>
           </div>
    
