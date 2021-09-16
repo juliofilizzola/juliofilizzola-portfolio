9e57f164bcdcdaec3f36148852b7f34c17592985
Filizzola <juliofilizzola@hotmail.com>
2021-09-16 11:30:49 -0300

    edit To-do List e colocando o projeto Shopping Cart

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -46,24 +46,23 @@ function Projects() {
             <div className="containerProject">
               <img className="img-projects" src={Todo} alt='imagem do projeto'/>
               <div className="text_Container">
-              <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe;</p>
+              <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe.</p>
               <p>
-                Esse projeto foi feito em React, usanado as tecnologia de React Router Dom, React Redux e foi estilizado usando CSS.
+                Esse projeto foi feito em React, usando as tecnologias: de React Router Dom, React Redux e foi estilizado usando CSS.
                 <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
               </p>
               </div>
             </div>
           </div>
           <div className="Project">
-            <h3 className="h3Project">Pokebola</h3> 
+            <h3 className="h3Project">Shopping Cart</h3> 
             <div className="containerProject">
               <div className="text_Container">
-                <p>O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end;
-
+                <p>O Shopping Cart foi um porjeto que foi feito baseado em um projeto da Trybe, do modulo de front-end. Consiste na manipulação de informações provinientes de uma API.
                 </p>
                 <p>
-                  Esse projeto foi feito em React, usnado as tecnologia de React Router Dom, React Redux e foi estilizado usando CSS.
-                  <a href="https://github.com/juliofilizzola/Pokebola" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
+                  Esse projeto foi feito em React, usando as tecnologias: React Router Dom, React Redux e Bootstrap.
+                  <a href="https://github.com/juliofilizzola/site-de-compras" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
                 </p>
                 </div>
                 <img className="img-projects" src={Pokebola} alt='imagem do projeto'/>
