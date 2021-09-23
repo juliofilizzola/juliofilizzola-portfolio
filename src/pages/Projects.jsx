cc5af681a83c463f3c8b599a7e3f113034156aa7
filizzola <juliofilizzola@hotmail.com>
2021-09-23 18:51:13 -0300

    fix: projects readme

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -46,7 +46,7 @@ function Projects() {
               <img className="img-projects" src={Todo} alt='imagem do projeto'/>
               <div className="text_Container">
               <p>
-                O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe.
+                O To-do List é o primeiro projeto pessoal usando React, fiz quando comecei o modulo de Front-end na Trybe.
 
               </p>
               <p>
@@ -60,7 +60,7 @@ function Projects() {
             <h3 className="h3Project">Shopping Cart</h3> 
             <div className="containerProject">
               <div className="text_Container">
-                <p>O Shopping Cart foi um porjeto que foi feito baseado em um projeto da Trybe, do modulo de front-end. Consiste na manipulação de informações provinientes de uma API.
+                <p>O Shopping Cart foi um porjeto que foi feito baseado em um projeto da Trybe, do modulo de front-end. Consiste na manipulação de informações provinientes de uma API do Mercado Livre.
                 </p>
                 <p>
                   Esse projeto foi feito em React, usando as tecnologias: React Router Dom, React Redux e Bootstrap.
@@ -77,12 +77,12 @@ function Projects() {
               <img className="img-projects" src={Avenges} alt='imagem do projeto'/>
               <div className="text_Container">
               <p>
-                O To-do List é o primeiro projeto pessoal, fiz quando comecei o modulo de Front-end na Trybe.
+                Esse foi um dos primeiros projetos que fiz usando API. Foi feito com o meu Amigo Rodolfo Rezende;
 
               </p>
               <p>
-                Esse projeto foi feito em React, usando as tecnologias: de React Router Dom, React Redux e foi estilizado usando CSS.
-                <a href="https://github.com/juliofilizzola/Project-todo-list" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
+                Usamos HTML e CSS nesse projeto.
+                <a href="https://github.com/juliofilizzola/Repo-Marvels" className="link_git" target="_blank" rel="noreferrer" >Link do Repositorio <DiGithubBadge/></a>
               </p>
               </div>
             </div>
