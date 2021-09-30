117fc7a3dbb82167f9275dd537509f7065751f77
filizzola <juliofilizzola@hotmail.com>
2021-09-29 22:19:34 -0300

    estruturando SocialNetworks|| edit contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -50,55 +50,7 @@ function Contacts() {
       <div className="container-contact">
         <h1>Contact Me</h1>
         <div className="contact">
-
-          <a 
-            href="https://www.linkedin.com/in/julio-filizzola/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            LinkedIn
-          </a>
-          <a
-            href="https://www.instagram.com/juliofilizzola/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-
-            Instagram
-          </a>
-          <a
-            href="https://twitter.com/Juliofilizzola"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-
-            Twitter
-          </a>
-          <a
-            href="https://github.com/juliofilizzola"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-
-            Github
-          </a>
-
+          
         </div> 
         <div className="container-contact-forms">
           <div className="separator">Ou me mande um email</div>
