641d28307cb4309e0a6cf6fd4f35f38bdca70511
Filizzola <juliofilizzola@hotmail.com>
2022-04-26 10:49:49 -0300

    💄 : criando arquivo de css

--- portifolio-filizzola/src/App.tsx
+++ portifolio-filizzola/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css';
+import './style/App/app.css';
 
 function App() {
   return (
