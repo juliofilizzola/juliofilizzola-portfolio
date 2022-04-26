10913f694fcab910ab75da17ff464132403056f9
Filizzola <juliofilizzola@hotmail.com>
2022-04-26 10:47:57 -0300

    ⚡️ : configurando primeiros arquivos

---
+++ portifolio-filizzola/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/about" element={ <About/> } />
+          <Route path="/projects" element={ <Projects/> } />
+          <Route path="/contacts" element={ <Contacts/> }/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
